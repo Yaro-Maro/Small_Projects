9c6eee6f3ba62aab27cf43d66011ebb6622c68b7
Yaro <y.larkin.91@gmail.com>
2021-09-06 14:12:13 +0200

    Lots of things added at work, began with JS.

--- Tip_Calculator/script.js
+++ Tip_Calculator/script.js
@@ -1,7 +1,17 @@
-function test() {
-  if (true) {
-    console.log("ROFL");
-    var drifting = "n00b";
-    drifting.length.number = 5;
-  }
+var billAmount = 100;
+var tipPercentage = 0.05;
+var numberOfPeople = 5;
+
+
+//Make $ an ID-selector.
+function $(name) {return document.getElementById(name)}
+
+function calculate() {
+  var tipPerPerson = (billAmount * tipPercentage)/numberOfPeople;
+  var billPerPerson = billAmount/numberOfPeople + tipPerPerson;
+
+  $("tipPerPerson").innerHTML = "$ " +  (12).toFixed(2);
+  $("billPerPerson").innerHTML = "$ " + billPerPerson;
 }
+
+calculate();
